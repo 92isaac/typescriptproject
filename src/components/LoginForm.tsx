53702205ddaeabb1198e0c8efee93879961790c6
Sangodare Isaac <sangodareisaac92@gmail.com>
2023-07-07 11:12:07 +0100

    add toastify to copytoclipboard

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -76,7 +76,8 @@ const LoginForm = () => {
     navigator.clipboard
       .writeText(text)
       .then(() => {
-        console.log("Text copied to clipboard");
+        // console.log("Text copied to clipboard");
+        toast.success('Text copied to clipboard')
       })
       .catch((error) => {
         console.error("Error copying text to clipboard:", error);
